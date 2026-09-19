92c7af623d2ab26a5ef847e9f78a59875d67ad2c
sonali surwade <sonalisurwade07@gmail.com>
2026-09-19 20:04:45 +0530

    deleting tasks from array

--- src/TodoList.jsx
+++ src/TodoList.jsx
@@ -6,12 +6,18 @@ export default function TodoList() {
   let [newTodo, setNewTodo] = useState("");
 
   let addNewTask = () => {
-    setTodos([...todos, { task: newTodo, id: uuidv4() }]);
+    setTodos((prevTodos) => {
+      return [...prevTodos, { task: newTodo, id: uuidv4() }];
+    });
     setNewTodo("");
   };
 
   let updateTodoValue = (event) => {
     setNewTodo(event.target.value);
+  };
+
+  let deleteTodo = (id) => {
+    setTodos(todos.filter((todo) => todo.id !== id));
   };
 
   return (
@@ -31,7 +37,11 @@ export default function TodoList() {
       <h4>Task Todo</h4>
       <ul>
         {todos.map((todo) => (
-          <li key={todo.id}>{todo.task}</li>
+          <li key={todo.id}>
+            <span>{todo.task}</span>
+            &nbsp;&nbsp;&nbsp;&nbsp;
+            <button onClick={() => deleteTodo(todo.id)}>delete</button>
+          </li>
         ))}
       </ul>
     </div>
