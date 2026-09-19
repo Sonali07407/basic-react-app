d4b3f3cad895b7798118449e22d4ecfe45bcdf30
sonali surwade <sonalisurwade07@gmail.com>
2026-09-19 17:17:57 +0530

    Add todo list empty string

--- src/TodoList.jsx
+++ src/TodoList.jsx
@@ -1,7 +1,38 @@
+import { useState } from "react";
+
 export default function TodoList() {
+  let [todos, setTodos] = useState(["Sample Task"]);
+  let [newTodo, setNewTodo] = useState("");
+
+  let addNewTask = () => {
+    setTodos([...todos, newTodo]);
+    setNewTodo("");
+  };
+
+  let updateTodoValue = (event) => {
+    setNewTodo(event.target.value);
+  };
+
   return (
     <div>
-      <h4>Todo List</h4>
+      <input
+        placeholder="add a task"
+        value={newTodo}
+        onChange={updateTodoValue}
+      ></input>
+      <br></br>
+      <button onClick={addNewTask}>Add task</button>
+      <br></br>
+      <br></br>
+      <br></br>
+
+      <hr></hr>
+      <h4>Task Todo</h4>
+      <ul>
+        {todos.map((todo) => (
+          <li>{todo}</li>
+        ))}
+      </ul>
     </div>
   );
 }
